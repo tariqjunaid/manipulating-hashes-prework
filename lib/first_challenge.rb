49099daeae093d634d471747d2ba6d836497728e
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-03-01 01:11:34 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,11 +12,11 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if { |flavor| flavor == "strawberry" }
+      end
+    end
+  end
 end
-
